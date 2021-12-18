6f940571c7307622095b6b95cb85ca17b9b69cfe
Fabian <fabian.zuo@supinternet.fr>
2021-12-18 14:41:41 +0100

    all project done

---
+++ README.md
@@ -0,0 +1,9 @@
+# This is a simple flask project for Christmas
+## Make your tree and have fun
+### How do you install the project ?
+1. First step is to initiate the project on your device by gettint this repository
+2. Then, do not forget installing venv with : ``` python3 -m venv .venv ```
+3. Activate it with : ``` source .venv/bin/activate ```
+4. Install Flask with : ``` pip install flask ```
+5. Launch the project : ``` python -m flask run ```
+6. Finally go on your browser and search the url given during the flask run and add ```/sapin/{the number you want between 0 and 30} and have fun with your tree
---
+++ app.py
@@ -0,0 +1,32 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+@app.route('/sapin/<size>')
+def tree(size):
+    size = int(size)
+    if size <= 0 or size > 30:
+        return "The size of your tree must be between 0 and 30"
+    tree_size = 4
+    line_size = 1
+    tree_result = []
+    trunk_result = ["|"]
+    trunk_thickness = 1
+    for block in range(1, size + 1):
+        if block % 2 == 0 :
+            trunk_result.append('|')
+            trunk_result.append('|')
+            trunk_thickness += 2
+        tree_temporary = []
+        for branch in range(1, tree_size +  1):
+            branch_temporary = []
+            for star in range(1, line_size + 1):
+                branch_temporary.append("*")
+            line_size += 2
+            tree_temporary.append(branch_temporary)
+        tree_result.append(tree_temporary)
+        if tree_size%2 == 0:
+            line_size = len(tree_temporary[int(tree_size/2)])
+        else :
+            line_size = len(tree_temporary[round(tree_size + 1 / 2) // 2])
+        tree_size += 1
+    return render_template('index.html', results=tree_result, trunks=trunk_result, trunk_width=size)
